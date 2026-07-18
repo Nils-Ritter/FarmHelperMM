9111e846cf7d73863ccf04067575814ddb7d5c33
Nils Ritter <mariebauernhuber@proton.me>
2026-07-18 14:49:53 +0200

    Implemented outline of AbstractMacro

--- src/main/java/com/jelly/farmhelperv3/macro/AbstractMacro.java
+++ src/main/java/com/jelly/farmhelperv3/macro/AbstractMacro.java
@@ -1,14 +1,16 @@
 package com.jelly.farmhelperv3.macro;
 
 import com.jelly.farmhelperv3.config.FarmHelperConfig;
+import com.jelly.farmhelperv3.handler.GameStateHandler;
 import com.jelly.farmhelperv3.util.helper.Clock;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.system.APIUtil;
 
 import java.util.Optional;
 
-public class AbstractMacro{
+public abstract class AbstractMacro{
     public static final Minecraft mc = Minecraft.getInstance();
     //private final RotationHandler rotation = RotationHandler.getInstance(); //TODO
     private final Clock rewarpDelay = new Clock();
@@ -39,6 +41,96 @@ public class AbstractMacro{
     private WalkingDirection walkingDirection = WalkingDirection.X;
     @Setter
     private int previousWalkingCoord = 0;
+
+    public boolean isEnabledAndNoFeature(){
+        //return enabled && !FeatureManager.getInstance().shouldPauseMacroExecution(); //TODO
+        return false;
+    }
+
+    public boolean isPaused(){
+        return !enabled;
+    }
+
+    private final Clock checkOnSpawnClock = new Clock();
+
+    private boolean sentWarning = false;
+
+    public boolean isYawSet(){
+        return yaw.isPresent();
+    }
+
+    public boolean isPitchSet(){
+        return pitch.isPresent();
+    }
+
+    public float getYaw(){
+        return yaw.orElse(0f);
+    }
+
+    public float getPitch(){
+        return pitch.orElse(0f);
+    }
+
+    public void setYaw(float yaw){
+        this.yaw = Optional.of(yaw);
+    }
+
+    public void setPitch(float pitch){
+        this.pitch = Optional.of(pitch);
+    }
+
+    public void onTick(){
+        //TODO
+    }
+
+    public void onLastRender(){ }
+    public void onChatMessageRecieved(String msg){ }
+    public void onOverlayRender(){ }
+    public void onPacketRecieved(){ }
+
+    public abstract void updateState();
+    public abstract void invokeState();
+
+    public void onEnable(){
+        //TODO
+    }
+
+    public void onDisable(){
+        //TODO
+    }
+
+    public void saveState(){
+        //TODO
+    }
+
+    public void changeState(){
+        //TODO
+    }
+
+    public abstract void actionAfterTeleport();
+
+    public State calculateDirection(){
+        //TODO
+        return State.NONE;
+    }
+
+    public void doAfterRewarpRotation(){
+        //TODO
+    }
+
+    protected void setWalkingDirection(){
+        //TODO
+    }
+
+    public boolean shouldRotateAfterWarp(){
+        //TODO
+        return false;
+    }
+
+    public void setBreakTime(double time, double timeBefore){
+        breakTime.schedule((long) time);
+        delayBeforeBreakTime.schedule((long) timeBefore);
+    }
 
     public enum State {
         // Add default values like NONE and DROPPING
